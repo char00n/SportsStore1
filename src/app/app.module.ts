02df9f4d1ca3fb5bbd7c8211649c296d5944d40c
Charon <ap.lisnevsky@gmail.com>
2019-01-28 00:12:11 +0200

    Sports Store - chapter 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [ BrowserModule, StoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
